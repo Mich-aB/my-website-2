0bf1ec514f2cd55ba327fa8c20eb2bf03ef52f2c
Mich-aB <micheledet08@gmail.com>
2026-01-20 11:42:33 +0100

    Add files via upload

---
+++ script.js/script.js
@@ -0,0 +1,10 @@
+
+     const menuToogle =
+   document.getElementById("menu-toogle");
+    const navLinks =
+   document.querySelelctor(".nav-links");
+    
+    menuToogle.addEventListener("click",()=>{
+       navLinks.classList.toogle("show");
+    });
+    
